f0b33f69a6ff69ce7363e618a3e28a52eef4ce1b
Gustavo Galupo <ggalupo2@hotmail.com>
2022-09-20 20:58:06 -0300

    Update extended backgroundImages on tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/galaxy-background.png')"
+        galaxy: "url('/galaxy-background.png')",
+        duo: 'linear-gradient(90.86deg, #9572FC 33.08%, #43E7AD 33.94%, #E1D55D 75.57%)'
       }
     },
   },
