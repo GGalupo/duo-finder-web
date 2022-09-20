e87c51802939bf1706ab409a018a5cd0abd600cc
Gustavo Galupo <ggalupo2@hotmail.com>
2022-09-20 00:50:09 -0300

    Initial home page styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import logoImg from "./assets/logo.svg";
+
+import "./styles/main.css";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} />
+    </div>
+  );
 }
 
 export default App;
