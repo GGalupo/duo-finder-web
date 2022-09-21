82cff6d7f2f3509b9b2957865413aa1dcb3d45d4
Gustavo Galupo <ggalupo2@hotmail.com>
2022-09-20 21:25:37 -0300

    Game grid

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,77 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Your{" "}
+        <span className="text-transparent bg-clip-text bg-duo-gradient">
+          duo
+        </span>{" "}
+        is here.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative rounded-lg overflow-hidden" href="">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/galaxy-background.png')",
-        duo: 'linear-gradient(90.86deg, #9572FC 33.08%, #43E7AD 33.94%, #E1D55D 75.57%)'
+        'galaxy': "url('/galaxy-background.png')",
+        'duo-gradient': 'linear-gradient(90.86deg, #9572FC 33.08%, #43E7AD 33.94%, #E1D55D 75.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
