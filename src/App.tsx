5b8e07a0738fa536c698a7d58058c5cccdc95f95
Gustavo Galupo <ggalupo2@hotmail.com>
2022-09-20 21:51:22 -0300

    Create "hasn't found your duo" section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 import logoImg from "./assets/logo.svg";
 
 import "./styles/main.css";
@@ -76,6 +78,24 @@ function App() {
             <span className="block text-zinc-300 text-sm mt-1">4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-duo-gradient self-stretch rounded-lg">
+        <div className="bg-[#2A2634] px-8 py-6 rounded-sm flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">
+              Hasn't found your duo?
+            </strong>
+            <p className="text-zinc-400 font-light">
+              Create your ad and find new players!
+            </p>
+          </div>
+
+          <button className="flex items-center gap-3 bg-violet-500 text-white px-4 py-3 rounded-md hover:bg-violet-600">
+            <MagnifyingGlassPlus size={24} />
+            Create new ad
+          </button>
+        </div>
       </div>
     </div>
   );
